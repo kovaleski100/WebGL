c5df49f2bcd9af2328fb9dba5422b20e4edf0bb7
Matheus <mfkovaleski@inf.ufrgs.br>
2022-09-08 01:18:59 -0300

    rotando a luz

--- js/scene/scene04.js
+++ js/scene/scene04.js
@@ -36,15 +36,53 @@ shadowLight.position.y = 4
 
 shadowLight.castShadow = true
 scene.add(shadowLight)
-
+x3.add(cube)
 x3.add(shadowLight)
 
 renderer.setAnimationLoop(() => {
 
+    var x = shadowLight.position.x    
+    var y = shadowLight.position.y
+    var z = shadowLight.position.z
+
+    var yaw = 0.02;
+    var pitch = 0;
+    var roll = 0;
+
+    var cosa = Math.cos(yaw);
+    var sina = Math.sin(yaw);
+
+    var cosb = Math.cos(pitch);
+    var sinb = Math.sin(pitch);
+
+    var cosc = Math.cos(roll);
+    var sinc = Math.sin(roll);
+
+    var Axx = cosa*cosb;
+    var Axy = cosa*sinb*sinc - sina*cosc;
+    var Axz = cosa*sinb*cosc + sina*sinc;
+
+    var Ayx = sina*cosb;
+    var Ayy = sina*sinb*sinc + cosa*cosc;
+    var Ayz = sina*sinb*cosc - cosa*sinc;
+
+    var Azx = -sinb;
+    var Azy = cosb*sinc;
+    var Azz = cosb*cosc;
+
+    x = Axx*x + Axy*y + Axz*z;
+    y = Ayx*x + Ayy*y + Ayz*z;
+    z = Azx*x + Azy*y + Azz*z;
+    
+    shadowLight.position.x = x
+    shadowLight.position.y = y
+    shadowLight.position.z = z
+
+
     cube.rotateY(0.01)
     cube.rotateX(0.01)
     cube.rotateZ(0.01)
-    x3.tick();
+    //x3.tick();
     x3.fps(()=>{
         renderer.render(scene,camera);
     });
